e0c2ac6c84492f15b16aa4ad1704c8a4fef5fba5
Alex Curran <alexcurran.va@gmail.com>
2021-03-11 15:43:17 -0600

    Installed basic dependencies for server, SQL db, and .env, also basic server.js setup is now functional

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
 "use strict";
+
+require("dotenv").config();
+
+const express = require("express");
+const PORT = process.env.PORT || 5000;
+// Import routers
+
+
+const app = express();
+
+if (process.env.NODE_ENV === "development") {
+    const morgan = require("morgan");
+
+    app.use(morgan("dev"));
+}
+
+// Middleware
+app.use(express.json());
+
+// API routers
+
+// General server error handler
+app.use((err, req, res, next) => {
+    console.error(err);
+
+    return res.status(500).json({ error: err.message });
+});
+
+app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode, and listening on PORT ${PORT}.`));

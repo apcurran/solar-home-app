aa513953d98820ce8521ead179c09bd484c07cb9
Alex Curran <alexcurran.va@gmail.com>
2021-03-12 12:53:04 -0600

    Removed one table and replaced with two independent tables for device types

--- db/db_schema.sql
+++ db/db_schema.sql
@@ -1,16 +1,21 @@
-CREATE TABLE solar_device (
+-- Premium device type
+CREATE TABLE solar_tile (
     id SERIAL PRIMARY KEY,
-    device_type VARCHAR(20) NOT NULL,
     tile_style VARCHAR(20),
-    price_per_500_sq_ft INT
+    price_per_500_sq_ft INT NOT NULL
+);
+
+-- Cheaper device type
+CREATE TABLE solar_panel (
+    id SERIAL PRIMARY KEY,
+    price_per_500_sq_ft INT NOT NULL
 );
 
 CREATE TABLE battery_pack (
     id SERIAL PRIMARY KEY,
-    price INT
+    price INT NOT NULL
 );
 
 CREATE TABLE user_order (
-    id SERIAL PRIMARY KEY,
-    
+    id SERIAL PRIMARY KEY
 );

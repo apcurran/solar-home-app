ec22883f2f740a7eda48f31220dbd788f7d1e7ea
Alex Curran <alexcurran.va@gmail.com>
2021-03-15 11:01:58 -0500

    Adjusted ids to be more table specific, and added a solar_device table

--- db/db_schema.sql
+++ db/db_schema.sql
@@ -1,25 +1,31 @@
+CREATE TABLE solar_device (
+    solar_device_id SERIAL PRIMARY KEY,
+    device_type_id INT,
+    FOREIGN KEY(device_type_id) REFERENCES solar_tile(solar_tile_id)
+);
+
 -- Premium device type
 CREATE TABLE solar_tile (
-    id SERIAL PRIMARY KEY,
+    solar_tile_id SERIAL PRIMARY KEY,
     tile_style VARCHAR(20) NOT NULL,
     price_per_500_sq_ft INT NOT NULL
 );
 
 -- Cheaper device type
 CREATE TABLE solar_panel (
-    id SERIAL PRIMARY KEY,
+    solar_panel_id SERIAL PRIMARY KEY,
     price_per_500_sq_ft INT NOT NULL
 );
 
 -- Accessory
 CREATE TABLE battery_pack (
-    id SERIAL PRIMARY KEY,
+    battery_pack_id SERIAL PRIMARY KEY,
     price INT NOT NULL
 );
 
 -- payment_id from Stripe payment
 CREATE TABLE user_order (
-    id SERIAL PRIMARY KEY,
+    user_order_id SERIAL PRIMARY KEY,
     customer_id SERIAL UNIQUE,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,

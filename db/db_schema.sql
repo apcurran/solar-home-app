f1f0d6e991b4b24497c529ff21927e2eb0ca5a6d
Alex Curran <alexcurran.va@gmail.com>
2021-03-15 13:55:08 -0500

    Changed order for passing to psql -f correctly

--- db/db_schema.sql
+++ db/db_schema.sql
@@ -1,13 +1,3 @@
-CREATE TABLE solar_device (
-    solar_device_id SERIAL PRIMARY KEY,
-    solar_tile_id INT,
-    FOREIGN KEY(solar_tile_id) REFERENCES solar_tile(solar_tile_id)
-    solar_panel_id INT,
-    FOREIGN KEY(solar_panel_id) REFERENCES solar_panel(solar_panel_id),
-    battery_pack_id INT,
-    FOREIGN KEY(battery_pack_id) REFERENCES battery_pack(battery_pack_id)
-);
-
 -- Premium device type
 CREATE TABLE solar_tile (
     solar_tile_id SERIAL PRIMARY KEY,
@@ -27,7 +17,17 @@ CREATE TABLE battery_pack (
     price INT NOT NULL
 );
 
--- payment_id from Stripe payment
+CREATE TABLE solar_device (
+    solar_device_id SERIAL PRIMARY KEY,
+    solar_tile_id INT,
+    FOREIGN KEY(solar_tile_id) REFERENCES solar_tile(solar_tile_id),
+    solar_panel_id INT,
+    FOREIGN KEY(solar_panel_id) REFERENCES solar_panel(solar_panel_id),
+    battery_pack_id INT,
+    FOREIGN KEY(battery_pack_id) REFERENCES battery_pack(battery_pack_id)
+);
+
+-- NOTE: payment_id from Stripe payment
 CREATE TABLE user_order (
     user_order_id SERIAL PRIMARY KEY,
     payment_id VARCHAR NOT NULL,
@@ -39,5 +39,5 @@ CREATE TABLE user_order (
     state CHAR(2) NOT NULL,
     zip CHAR(5) NOT NULL,
     order_total INT NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL
 );

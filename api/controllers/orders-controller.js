0545ba991a1bd13e97ad089c5267050a14d79729
Alex Curran <alexcurran.va@gmail.com>
2021-03-16 12:24:08 -0500

    Added SQL query for getOrder controller func

--- api/controllers/orders-controller.js
+++ api/controllers/orders-controller.js
@@ -3,7 +3,19 @@
 const db = require("../../db/index");
 
 async function getOrder(req, res, next) {
-
+    try {
+        const { orderId } = req.body;
+        const ordersInfo = (await db.query(`
+            SELECT *
+            FROM customer_order
+            WHERE customer_order.customer_order_id = $1
+        `, [orderId])).rows;
+    
+        res.json(ordersInfo);
+        
+    } catch (err) {
+        next(err);
+    }
 }
 
 module.exports = {

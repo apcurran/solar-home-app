afd42a3051f811b93c75362b2c93677c63744d9d
Alex Curran <alexcurran.va@gmail.com>
2021-03-17 16:45:13 -0500

    Added bg img and bg blend mode with fill color multiplied

--- client/src/components/views/Home.js
+++ client/src/components/views/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import "./Home.css";
+
 function Home() {
     return (
         <div className="home">
-            
+            <section className="home__hero"></section>
+            {/* <figure className="home__fig">
+                <img src="https://res.cloudinary.com/dev-project/image/upload/f_auto,q_auto/v1616015507/aperture-solar/hero-solar-roof_rkxfsc.jpg" alt="Solar tiles on angled roof" className="home__fig__img"/>
+            </figure> */}
         </div>
     );
 }

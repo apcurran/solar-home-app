69a85f3c89779fb90a07e33395ad5b5a45745bca
Alex Curran <alexcurran.va@gmail.com>
2021-03-17 14:48:20 -0500

    Added components folders, Header comp, and Home comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Header from "./components/layout/Header";
+import Home from "./components/views/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/" component={Home} exact />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 

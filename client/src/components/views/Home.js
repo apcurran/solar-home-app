7f43af795be9aaff20d352eba2ca721127c9b303
Alex Curran <alexcurran.va@gmail.com>
2021-03-18 10:17:21 -0500

    Used Grid for placement of header text instead of absolute positioning

--- client/src/components/views/Home.js
+++ client/src/components/views/Home.js
@@ -9,7 +9,10 @@ function Home() {
             <Header headerHome = "header-home" />
             <div className="home">
                 <section className="home__hero">
-
+                    <section className="home__hero__title-sect">
+                        <h1 className="home__hero__title">Solar</h1>
+                        <h1 className="home__hero__title--subheading">For Your Home</h1>
+                    </section>
                 </section>
             </div>
         </>

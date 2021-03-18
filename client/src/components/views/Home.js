3479e542c9b3062b6f8b3654392d14e2539235ff
Alex Curran <alexcurran.va@gmail.com>
2021-03-18 14:36:22 -0500

    Added img dimensions for width/height and lazy loading

--- client/src/components/views/Home.js
+++ client/src/components/views/Home.js
@@ -24,14 +24,14 @@ function Home() {
                         </div>
                         <div className="home__main__sect__inner-sect-visual">
                             <figure className="home__main__sect__inner-sect-visual__fig">
-                                <img  className="home__main__sect__inner-sect-visual__fig__img" src="https://res.cloudinary.com/dev-project/image/upload/f_auto,q_auto/v1616083916/aperture-solar/house-solar-panels_szr0wk.jpg" alt="Two-story house with solar panels on the roof."/>
+                                <img  className="home__main__sect__inner-sect-visual__fig__img" src="https://res.cloudinary.com/dev-project/image/upload/f_auto,q_auto/v1616083916/aperture-solar/house-solar-panels_szr0wk.jpg" alt="Two-story house with solar panels on the roof." width="1920" height="1280" loading="lazy"/>
                             </figure>
                         </div>
                     </section>
                     <section className="home__main__sect">
                         <div className="home__main__sect__inner-sect-visual">
                             <figure className="home__main__sect__inner-sect-visual__fig">
-                                <img  className="home__main__sect__inner-sect-visual__fig__img" src="https://res.cloudinary.com/dev-project/image/upload/f_auto,q_auto/v1616093154/aperture-solar/solar-panel-infinity_w0rzmd.jpg" alt="View of solar panels angled upwards into the blue sky."/>
+                                <img  className="home__main__sect__inner-sect-visual__fig__img" src="https://res.cloudinary.com/dev-project/image/upload/f_auto,q_auto/v1616093154/aperture-solar/solar-panel-infinity_w0rzmd.jpg" alt="View of solar panels angled upwards into the blue sky." width="1920" height="1280" loading="lazy"/>
                             </figure>
                         </div>
                         <div className="home__main__sect__inner-sect-content">
@@ -50,14 +50,14 @@ function Home() {
                         </div>
                         <div className="home__main__sect__inner-sect-visual">
                             <figure className="home__main__sect__inner-sect-visual__fig">
-                                <img  className="home__main__sect__inner-sect-visual__fig__img" src="https://res.cloudinary.com/dev-project/image/upload/f_auto,q_auto/v1616094372/aperture-solar/quality-construction-solar-panels_pbvp4h.jpg" alt="Construction workers installing solar panels."/>
+                                <img  className="home__main__sect__inner-sect-visual__fig__img" src="https://res.cloudinary.com/dev-project/image/upload/f_auto,q_auto/v1616094372/aperture-solar/quality-construction-solar-panels_pbvp4h.jpg" alt="Construction workers installing solar panels." width="1920" height="877" loading="lazy"/>
                             </figure>
                         </div>
                     </section>
                     <section className="home__main__sect">
                         <div className="home__main__sect__inner-sect-visual">
                             <figure className="home__main__sect__inner-sect-visual__fig">
-                                <img  className="home__main__sect__inner-sect-visual__fig__img" src="https://res.cloudinary.com/dev-project/image/upload/f_auto,q_auto/v1616094373/aperture-solar/roof-tiles_frr2i6.jpg" alt="Construction workers installing solar panels."/>
+                                <img  className="home__main__sect__inner-sect-visual__fig__img" src="https://res.cloudinary.com/dev-project/image/upload/f_auto,q_auto/v1616094373/aperture-solar/roof-tiles_frr2i6.jpg" alt="Construction workers installing solar panels." width="1920" height="2880" loading="lazy"/>
                             </figure>
                         </div>
                         <div className="home__main__sect__inner-sect-content">

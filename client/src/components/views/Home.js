529be45338aaac5a99ea8d5cb148bb0227577bf6
Alex Curran <alexcurran.va@gmail.com>
2021-03-18 13:50:05 -0500

    Added img

--- client/src/components/views/Home.js
+++ client/src/components/views/Home.js
@@ -31,11 +31,11 @@ function Home() {
                     <section className="home__main__sect">
                         <div className="home__main__sect__inner-sect-visual">
                             <figure className="home__main__sect__inner-sect-visual__fig">
-                                <img  className="home__main__sect__inner-sect-visual__fig__img" src="https://res.cloudinary.com/dev-project/image/upload/f_auto,q_auto/v1616083916/aperture-solar/house-solar-panels_szr0wk.jpg" alt="Two-story house with solar panels on the roof."/>
+                                <img  className="home__main__sect__inner-sect-visual__fig__img" src="https://res.cloudinary.com/dev-project/image/upload/f_auto,q_auto/v1616093154/aperture-solar/solar-panel-infinity_w0rzmd.jpg" alt="View of solar panels angled upwards into the blue sky."/>
                             </figure>
                         </div>
                         <div className="home__main__sect__inner-sect-content">
-                            <h2 className="home__main__sect__inner-sect__title home__main__sect__inner-sect__title--light">Choose Solar Tiles or Panels</h2>
+                            <h2 className="home__main__sect__inner-sect__title home__main__sect__inner-sect__title--light">Pay Less for Electricity</h2>
                             <p className="home__main__sect__inner-sect__desc home__main__sect__inner-sect__desc--light">
                                 Phasellus sit amet varius neque. Fusce mattis erat at est mattis, sed gravida lorem maximus. Phasellus eget pharetra nibh. Aliquam ex lectus, commodo vel tortor in, interdum molestie neque. Curabitur laoreet, sapien nec porta faucibus, risus diam finibus dolor, quis convallis velit est eget libero.
                             </p>

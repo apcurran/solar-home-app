0e3cb6b6d68a048486b1d6ad2175103e98ea2fd9
Alex Curran <alexcurran.va@gmail.com>
2021-03-18 11:26:10 -0500

    Added first main sect

--- client/src/components/views/Home.js
+++ client/src/components/views/Home.js
@@ -14,6 +14,21 @@ function Home() {
                         <h1 className="home__hero__title--subheading">For Your Home</h1>
                     </section>
                 </section>
+                <main className="home__main">
+                    <section className="home__main__sect">
+                        <div className="home__main__sect__inner-sect--left">
+                            <h2 className="home__main__sect__inner-sect__title">Choose Solar Tiles or Panels</h2>
+                            <p className="home__main__sect__inner-sect__desc">
+                                Phasellus sit amet varius neque. Fusce mattis erat at est mattis, sed gravida lorem maximus. Phasellus eget pharetra nibh. Aliquam ex lectus, commodo vel tortor in, interdum molestie neque. Curabitur laoreet, sapien nec porta faucibus, risus diam finibus dolor, quis convallis velit est eget libero.
+                            </p>
+                        </div>
+                        <div className="home__main__sect__inner-sect--right">
+                            <figure className="home__main__sect__inner-sect--right__fig">
+                                <img  className="home__main__sect__inner-sect--right__fig__img" src="https://res.cloudinary.com/dev-project/image/upload/f_auto,q_auto/v1616083916/aperture-solar/house-solar-panels_szr0wk.jpg" alt="Two-story house with solar panels on the roof."/>
+                            </figure>
+                        </div>
+                    </section>
+                </main>
             </div>
         </>
     );

b7826d721b3e837383ad29bfce07f075aacf27ac
Alex Curran <alexcurran.va@gmail.com>
2021-03-25 11:00:59 -0500

    Styling elems for 50% width inputs

--- client/src/components/views/design/customizer-ui/CustomizerUi.js
+++ client/src/components/views/design/customizer-ui/CustomizerUi.js
@@ -21,12 +21,33 @@ function CustomizerUi() {
                 <span className="design__customizer__form__title-note">(Optional)</span>
                 <div className="design__customizer__form__controls-container-outer-wrapper">
                     <div className="design__customizer__form__controls-container">
-                        <label htmlFor="battery" className="design__customizer__form__label">Number of Battery Packs</label>
-                        <input type="number" name="battery" id="battery" className="design__customizer__form__input battery-input"/>
+                        <label htmlFor="battery" className="design__customizer__form__label">Battery Packs Qty.</label>
+                        <input type="number" name="battery" id="battery" className="design__customizer__form__input battery-input" maxLength="1"/>
                     </div>
                     <div className="design__customizer__form__controls-container">
                         <button className="design__customizer__form__btn" type="button">Add</button>
                     </div>
+                </div>
+                <h1 className="design__customizer__form__title">Personal Info</h1>
+                <div className="design__customizer__form__controls-container">
+                    <label htmlFor="first-name" className="design__customizer__form__label">First Name</label>
+                    <input type="text" name="first-name" id="first-name" className="design__customizer__form__input" required/>
+                </div>
+                <div className="design__customizer__form__controls-container">
+                    <label htmlFor="last-name" className="design__customizer__form__label">Last Name</label>
+                    <input type="text" name="last-name" id="last-name" className="design__customizer__form__input" required/>
+                </div>
+                <div className="design__customizer__form__controls-container">
+                    <label htmlFor="street" className="design__customizer__form__label">Street Address</label>
+                    <input type="text" name="street" id="street" className="design__customizer__form__input" required/>
+                </div>
+                <div className="design__customizer__form__controls-container">
+                    <label htmlFor="state" className="design__customizer__form__label">State <span className="design__customizer__form__label-note">(2 Characters)</span></label>
+                    <input type="text" name="state" id="state" className="design__customizer__form__input col--half-width" minLength="2" maxLength="2" required/>
+                </div>
+                <div className="design__customizer__form__controls-container">
+                    <label htmlFor="zip" className="design__customizer__form__label">Zip Code <span className="design__customizer__form__label-note">(5 Characters)</span></label>
+                    <input type="number" name="zip" id="zip" className="design__customizer__form__input col--half-width" minLength="5" maxLength="5" required/>
                 </div>
             </form>
         </main>

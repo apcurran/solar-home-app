b7dda6b88ee21504b60d33cb1dd21be6bef6b7c2
Alex Curran <alexcurran.va@gmail.com>
2021-03-25 11:49:04 -0500

    Finished base form

--- client/src/components/views/design/customizer-ui/CustomizerUi.js
+++ client/src/components/views/design/customizer-ui/CustomizerUi.js
@@ -1,6 +1,17 @@
+import { useState } from "react";
+
 import "./CustomizerUi.css";
 
 function CustomizerUi() {
+    const [phone, setPhone] = useState("");
+
+    function phoneHandler(event) {
+        const inputVal = event.target.value;
+        const validatedInputval = inputVal.replace(/[^0-9]/g, "");
+        
+        setPhone(validatedInputval);
+    }
+
     return (
         <main className="design__customizer">
             <form className="design__customizer__form">
@@ -48,6 +59,43 @@ function CustomizerUi() {
                 <div className="design__customizer__form__controls-container">
                     <label htmlFor="zip" className="design__customizer__form__label">Zip Code <span className="design__customizer__form__label-note">(5 Characters)</span></label>
                     <input type="number" name="zip" id="zip" className="design__customizer__form__input col--half-width" minLength="5" maxLength="5" required/>
+                </div>
+                <div className="design__customizer__form__controls-container">
+                    <label htmlFor="email" className="design__customizer__form__label">Email</label>
+                    <input type="email" name="email" id="email" className="design__customizer__form__input" required/>
+                </div>
+                <div className="design__customizer__form__controls-container">
+                    <label htmlFor="phone" className="design__customizer__form__label">Phone Number</label>
+                    <input onChange={phoneHandler} type="tel" name="phone" id="phone" className="design__customizer__form__input" required/>
+                </div>
+                <div className="design__customizer__form__controls-container">
+                    <label htmlFor="home-size" className="design__customizer__form__label">Home Size <span className="design__customizer__form__label-note">(sq. ft.)</span></label>
+                    <input type="number" name="home-size" id="home-size" className="design__customizer__form__input col--half-width" required/>
+                </div>
+                <h1 className="design__customizer__form__title">Payment Details</h1>
+                <div className="design__customizer__form__controls-container">
+                    <label htmlFor="card-name" className="design__customizer__form__label">Name on Card</label>
+                    <input type="text" name="card-name" id="card-name" className="design__customizer__form__input" required/>
+                </div>
+                <div className="design__customizer__form__controls-container">
+                    <label htmlFor="card-number" className="design__customizer__form__label">Card Number</label>
+                    <input type="number" name="card-number" id="card-number" className="design__customizer__form__input" required/>
+                </div>
+                <div className="design__customizer__form__controls-container">
+                    <label htmlFor="card-expiration-month" className="design__customizer__form__label">Expiration Month</label>
+                    <input type="text" name="card-expiration-month" id="card-expiration-month" className="design__customizer__form__input col--half-width" required/>
+                </div>
+                <div className="design__customizer__form__controls-container">
+                    <label htmlFor="card-expiration-year" className="design__customizer__form__label">Expiration Year</label>
+                    <input type="number" name="card-expiration-year" id="card-expiration-year" className="design__customizer__form__input col--half-width" minLength="4" maxLength="4" required/>
+                </div>
+                <div className="design__customizer__form__controls-container">
+                    <label htmlFor="card-expiration-cvv" className="design__customizer__form__label">CVV</label>
+                    <input type="number" name="card-expiration-cvv" id="card-expiration-cvv" className="design__customizer__form__input col--half-width" minLength="3" maxLength="3" required/>
+                </div>
+                <div className="design__customizer__form__controls-container">
+                    <label htmlFor="billing-zip" className="design__customizer__form__label">Billing Zip Code</label>
+                    <input type="number" name="billing-zip" id="billing-zip" className="design__customizer__form__input col--half-width" minLength="5" maxLength="5" required/>
                 </div>
             </form>
         </main>

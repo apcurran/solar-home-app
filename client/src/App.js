f7de55bb67d4f0bbdb2d8ff5a4d7f2eed624359a
Alex Curran <alexcurran.va@gmail.com>
2021-03-29 15:58:25 -0500

    Moved loadStripe and Elements into App.js to wrap all routes to benefit from Stripe's security measures

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,22 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
 import Home from "./components/views/home/Home";
 import Design from "./components/views/design/Design";
+
+const stripePromise = loadStripe("pk_test_51GyOBXEFSgPrRFzWmUBqUfL9lNgsZR7qWlzKEQe7hTRs48845plWirspdaEqDps99uKE3MumJXsyjJPlSXoCGgOR0089tYnp4V");
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/design" component={Design} exact />
-        </Switch>
+        <Elements stripe={stripePromise}>
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/design" component={Design} exact />
+          </Switch>
+        </Elements>
       </div>
     </Router>
   );

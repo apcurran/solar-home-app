9a3ce38d5bcee93a9f7e9b2308e68e9123446e0d
Alex Curran <alexcurran.va@gmail.com>
2021-03-31 14:45:12 -0500

    Deleted some unnecessary comments and old code

--- client/src/components/views/design/customizer-ui/CustomizerUi.js
+++ client/src/components/views/design/customizer-ui/CustomizerUi.js
@@ -112,7 +112,6 @@ function CustomizerUi({ handleImgChange }) {
         const myItems = createItemsArr();
 
         try {
-            // Make fetch req
             const response = await fetch("/api/orders/create-payment-intent", {
                 method: "POST",
                 headers: {
@@ -148,7 +147,6 @@ function CustomizerUi({ handleImgChange }) {
     }
 
     function handleStripeCardChange(event) {
-        // Display any errs
         setErrorMessage(event.error ? event.error.message : "");
     }
 
@@ -213,30 +211,6 @@ function CustomizerUi({ handleImgChange }) {
                 <h1 className="design__customizer__form__title">Payment Details</h1>
                 <CardElement options={cardElementOptions} onChange={handleStripeCardChange} />
                 <button disabled={isProcessing || successMessage} className="design__customizer__form__submit-btn" type="submit">{isProcessing ? "Processing" : "Pay Now"}</button>
-                {/* <div className="design__customizer__form__controls-container">
-                    <label htmlFor="card-name" className="design__customizer__form__label">Name on Card</label>
-                    <input type="text" name="card-name" id="card-name" className="design__customizer__form__input" required/>
-                </div> */}
-                {/* <div className="design__customizer__form__controls-container">
-                    <label htmlFor="card-number" className="design__customizer__form__label">Card Number</label>
-                    <input type="number" name="card-number" id="card-number" className="design__customizer__form__input" required/>
-                </div>
-                <div className="design__customizer__form__controls-container">
-                    <label htmlFor="card-expiration-month" className="design__customizer__form__label">Expiration Month</label>
-                    <input type="text" name="card-expiration-month" id="card-expiration-month" className="design__customizer__form__input col--half-width" required/>
-                </div>
-                <div className="design__customizer__form__controls-container">
-                    <label htmlFor="card-expiration-year" className="design__customizer__form__label">Expiration Year</label>
-                    <input type="number" name="card-expiration-year" id="card-expiration-year" className="design__customizer__form__input col--half-width" minLength="4" maxLength="4" required/>
-                </div>
-                <div className="design__customizer__form__controls-container">
-                    <label htmlFor="card-expiration-cvv" className="design__customizer__form__label">CVV</label>
-                    <input type="number" name="card-expiration-cvv" id="card-expiration-cvv" className="design__customizer__form__input col--half-width" minLength="3" maxLength="3" required/>
-                </div>
-                <div className="design__customizer__form__controls-container">
-                    <label htmlFor="billing-zip" className="design__customizer__form__label">Billing Zip Code</label>
-                    <input type="number" name="billing-zip" id="billing-zip" className="design__customizer__form__input col--half-width" minLength="5" maxLength="5" required/>
-                </div> */}
             </form>
             {successMessage ? <p className="message message--success">{successMessage}</p> : null}
             {errorMessage ? <p className="message message--error">{errorMessage}</p> : null}
